503db67f90ded0d4c0f57341af53293c34ceabab
piyushsrii <piyushnamo888@gmail.com>
2020-07-12 15:00:26 +0530

    merge uc2

--- rule.sh
+++ rule.sh
@@ -8,3 +8,21 @@ echo "Employee is present"
 else
 echo "Employee is not present"
 fi
+
+
+#!/bin/bash -x
+ isPartTime=1;
+ isFullTime=2;
+ empRatePerHr=20;
+ randomCheck=$((RANDOM%3));
+     if [ $isFullTime -eq $randomCheck ];
+then
+        empHrs=8;
+elif     [ $isFullTime -eq $randomCheck ];
+then
+           empHrs=4;
+else
+         empHrs=0;
+fi
+    salary=$(($empHrs*$empRatePerHr));
+      echo $salary
